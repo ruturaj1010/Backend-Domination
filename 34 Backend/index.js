5da35475bbf5507e733906209f32020ba3fedaad
Ruturaj <ruturajnikam1010@gmail.com>
2025-02-12 20:57:17 +0530

    Created uuid for post

--- 34 Backend/index.js
+++ 34 Backend/index.js
@@ -2,6 +2,7 @@ const express =  require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 
 app.use(express.urlencoded({extended:true}));
 
@@ -13,14 +14,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
+        id : uuidv4(),
         username :"ruturaj1010",
         content : "Love to code more and more!"
     },
     {
+        id : uuidv4(),
         username :"sakshi0208",
         content : "Love Cooking!"
     },
     {
+        id : uuidv4(),
         username :"sunita1204",
         content : "Unwavering love for fashion designing!"
     }
@@ -37,8 +41,18 @@ app.get("/posts/new", (req, res)=>{
 app.post("/posts", (req, res)=>{
     // console.log(req.body);
     let { username, content } =  req.body;
-    posts.push({ username , content})
+    let id = uuidv4();
+    posts.push({ id, username , content})
     res.redirect("/posts");
+})
+
+app.get("/posts/:id", (req, res)=>{
+    const { id } = req.params;
+    const post = posts.find(post => id === post.id);
+    console.log(post)
+
+    // res.send(`Your id is ${id}`);
+    res.render("show.ejs", { post })
 })
 
 app.listen(port , ()=>{
