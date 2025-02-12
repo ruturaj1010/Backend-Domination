890876eda9c89de77d4db7043af0251ebda41ef1
Ruturaj <ruturajnikam1010@gmail.com>
2025-02-12 23:38:20 +0530

    Committed the patch and delete request

--- 34 Backend/index.js
+++ 34 Backend/index.js
@@ -1,10 +1,13 @@
-const express =  require("express");
+const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
+var methodOverride = require('method-override')
 
-app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'))
+
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -14,39 +17,39 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
-        id : uuidv4(),
-        username :"ruturaj1010",
-        content : "Love to code more and more!"
+        id: uuidv4(),
+        username: "ruturaj1010",
+        content: "Love to code more and more!"
     },
     {
-        id : uuidv4(),
-        username :"sakshi0208",
-        content : "Love Cooking!"
+        id: uuidv4(),
+        username: "sakshi0208",
+        content: "Love Cooking!"
     },
     {
-        id : uuidv4(),
-        username :"sunita1204",
-        content : "Unwavering love for fashion designing!"
+        id: uuidv4(),
+        username: "sunita1204",
+        content: "Unwavering love for fashion designing!"
     }
 ]
 
-app.get("/posts", (req, res)=>{
+app.get("/posts", (req, res) => {
     res.render("index.ejs", { posts });
 })
 
-app.get("/posts/new", (req, res)=>{
+app.get("/posts/new", (req, res) => {
     res.render("form.ejs")
 })
 
-app.post("/posts", (req, res)=>{
+app.post("/posts", (req, res) => {
     // console.log(req.body);
-    let { username, content } =  req.body;
+    let { username, content } = req.body;
     let id = uuidv4();
-    posts.push({ id, username , content})
+    posts.push({ id, username, content })
     res.redirect("/posts");
 })
 
-app.get("/posts/:id", (req, res)=>{
+app.get("/posts/:id", (req, res) => {
     const { id } = req.params;
     const post = posts.find(post => id === post.id);
     console.log(post)
@@ -55,6 +58,30 @@ app.get("/posts/:id", (req, res)=>{
     res.render("show.ejs", { post })
 })
 
-app.listen(port , ()=>{
+app.patch("/posts/:id", (req, res) => {
+    const { id } = req.params;
+    console.log(id)
+
+    const newcontent = req.body.content;
+    const post = posts.find(post => id === post.id);
+    post.content = newcontent;
+    console.log(post);
+    res.redirect(`/posts`)
+})
+
+app.get("/posts/:id/edit", (req, res) => {
+    const { id } = req.params;
+    const post = posts.find(post => id === post.id);
+    res.render("edit.ejs", { post })
+})
+
+app.delete("/posts/:id" , (req, res)=>{
+    const {id} = req.params;
+    posts = posts.filter(post => id !== post.id);
+    console.log(posts);
+    res.redirect("/posts");
+})
+
+app.listen(port, () => {
     console.log(`Server is listening on port : ${port}`);
 })
