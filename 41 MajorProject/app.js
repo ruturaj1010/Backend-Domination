ad7b862c5e3ec36b7786f35faa7ed0567401d832
Ruturaj <ruturajnikam1010@gmail.com>
2025-02-19 15:06:17 +0530

    completed with update and delete part  of project

--- 41 MajorProject/app.js
+++ 41 MajorProject/app.js
@@ -3,10 +3,15 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require('ejs-mate');
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
 
 main().then(res => {
     console.log("mongoose connected successfully");
@@ -57,6 +62,25 @@ app.post("/listings", async (req, res) => {
     // let listing = req.body.listing;
     let newListing = new Listing(req.body.listing);
     await newListing.save();
+    res.redirect("/listings");
+})
+
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+})
+
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+app.delete("/listings/:id/delete" , async (req , res)=>{
+    let { id } = req.params;
+    let deletedlist = await Listing.findByIdAndDelete(id);
+    console.log(deletedlist)
     res.redirect("/listings");
 })
 
