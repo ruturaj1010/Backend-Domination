e4f47d78ab2b7552485ad3156104ae2d628ee614
Ruturaj <ruturajnikam1010@gmail.com>
2025-03-03 01:59:33 +0530

    applied authorization and authentication using passport

--- 41 MajorProject/app.js
+++ 41 MajorProject/app.js
@@ -7,12 +7,15 @@ const ejsMate = require('ejs-mate');
 const ExpressError = require("./utilty/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
-
-main().then( res => {
+main().then(res => {
     console.log("mongoose connected successfully");
 }).catch(err => {
     console.log(err);
@@ -31,27 +34,45 @@ app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 const sessionOptions = {
-    secret : "mysupersecretcode",
-    resave : false,
-    saveUninitialized : true,
-    cookie : {
-        expires : Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge : 7 * 24 * 60 * 60 * 1000,
-        httpOnly : true,
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
     },
 };
 
 app.use(session(sessionOptions));
 app.use(flash())
 
-app.use((req, res , next)=>{
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews" , reviews )
+// app.get("/demouser" , async(req, res)=> {
+//     let fakeUser = new User({
+//         email : "user@2122",
+//         username : "user321"
+//     });
+
+//     let registeredUser = await User.register(fakeUser , "ruturajsecret");
+//     res.send(registeredUser);
+// })
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/" , userRouter);
 
 app.get("*", (req, res, next) => {
     next(new ExpressError(404, "page not found"));
